e003dc3462682eec69fd6e0cae25b6b55c862ac4
benstew <benstewart2008@gmail.com>
2015-11-28 17:14:03 -0500

    Add about page and update tests for static_pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -8,6 +9,11 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get help" do
     get :help
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
     assert_response :success
   end
 
