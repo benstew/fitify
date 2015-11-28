2b447c547c577dcf1ae7ab754315bb78ce232fc4
benstew <benstewart2008@gmail.com>
2015-11-28 17:30:26 -0500

    Finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Social Share Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Social Share Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Social Share Sample App"
   end
 
 end
