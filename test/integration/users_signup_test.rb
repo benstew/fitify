9832559580a84e1e0a32b7456b1a502e47f32d95
benstew <benstewart2008@gmail.com>
2015-11-29 10:35:11 -0500

    Updated controller so that user is logged in when they sign up and have an accompanying integration test for it

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
     # assert_not flash.FIll_IN #for testing the flash message
+    assert is_logged_in?
   end
 end
