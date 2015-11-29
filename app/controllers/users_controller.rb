c8bc2bffa181f7abb90c1201bcf909c54cde7d1b
benstew <benstewart2008@gmail.com>
2015-11-29 08:39:50 -0500

    Added error message partial to users signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def created
+  def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save.
