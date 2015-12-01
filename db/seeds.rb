82d5fabeb9b960097f5db21e687a471d2f808e14
benstew <benstewart2008@gmail.com>
2015-11-30 20:20:03 -0500

    Added faker gem and seed file so that index action wouldn't be sad

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+# reset the database with rake db:migrate:reset
+# To clean User.destory_all
+
+User.create!(name:  "Example User",
+             email: "example@gmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
