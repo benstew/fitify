aa70d873334c011f01a9e1c7b1c8625f9a02d8d6
benstew <benstewart2008@gmail.com>
2015-11-30 20:37:56 -0500

    Udated seedfile to inlcude an admin and added admin column to user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@
 User.create!(name:  "Example User",
              email: "example@gmail.com",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
