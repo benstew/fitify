f44eb9200407cfc7ab64597315d62916b0fc1600
benstew <benstewart2008@gmail.com>
2015-11-30 19:47:45 -0500

    Update user controller and integration test for edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 
   private
 
