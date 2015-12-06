1cc8e99c72108e2d5e4e76506e1d603e020ecf94
benstew <benstewart2008@gmail.com>
2015-12-06 10:17:39 -0500

    Updated Micropost pagination, integration tests, and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   # Callback to downcase the email attribute before saving the user
   before_save   :downcase_email
