2cd11aaf137f102c57f05a5497724b401acc429c
benstew <benstewart2008@gmail.com>
2015-12-06 10:22:48 -0500

    Updated micropost controller to have create/destroy, moved logged_in? method to applciation controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,22 +53,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
-  end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
-
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
