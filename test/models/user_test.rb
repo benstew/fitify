ded63fae030c1bd0fe853c8235fe0b3c15e909ad
benstew <benstewart2008@gmail.com>
2015-12-07 12:51:06 -0500

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -89,4 +89,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not ben.following?(archer)
   end
 
+  test "feed should have the right posts" do
+    ben = users(:ben)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert ben.feed.include?(post_following)
+    end
+    # Posts from self
+    ben.microposts.each do |post_self|
+      assert ben.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not ben.feed.include?(post_unfollowed)
+    end
+  end
+
 end
