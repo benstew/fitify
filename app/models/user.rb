750f4639b85fdced7d6ad9121819029db856eadf
benstew <benstewart2008@gmail.com>
2015-12-13 15:25:03 -0500

    Changed name to Workoutify and updated CSS for homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
+
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",
                                 foreign_key: "follower_id",
@@ -24,6 +25,8 @@ class User < ActiveRecord::Base
 
 has_secure_password
 validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+
 
   # Returns the hash digest of the given string.
   def User.digest(string)
